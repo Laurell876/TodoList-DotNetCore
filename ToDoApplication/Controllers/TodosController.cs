ad2f83ea8e69c2da9bd307a7e2d8dbc6090deb30
Laurell Seville <laurellseville2000@gmail.com>
2020-08-27 21:15:19 -0500

    display all todos

---
+++ ToDoApplication/Controllers/TodosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoApplication.Models;
+
+namespace ToDoApplication.Controllers
+{
+    public class TodosController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TodosController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // Get Todos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Todos.ToListAsync());
+        }
+
+
+    }
+}
--- ToDoApplication/Startup.cs
+++ ToDoApplication/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ToDoApplication.Models;
 
 namespace ToDoApplication
 {
@@ -23,7 +25,9 @@ namespace ToDoApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
